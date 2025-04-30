b69b3fb08293e501cc31fa6ca210b7c9428cb19b
michael <mikerozendo@gmail.com>
2025-04-30 16:39:18 -0300

    Refactor run.sh to handle directory arguments and ensure the directory exists before execution

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
-#
-# This script is used to run your program on CodeCrafters
-#
-# This runs after .codecrafters/compile.sh
-#
-# Learn more: https://codecrafters.io/program-interface
 
-set -e # Exit on failure
+# Exit on failure
+set -e
 
-exec /tmp/codecrafters-build-http-server-csharp/codecrafters-http-server "$@"
+# Default directory (in case no --directory is provided)
+DEFAULT_DIRECTORY="/tmp"
+
+# Parse arguments
+DIRECTORY="$DEFAULT_DIRECTORY"
+for ARG in "$@"; do
+  case $ARG in
+    --directory=*)
+      DIRECTORY="${ARG#*=}"
+      ;;
+  esac
+done
+
+# Ensure the directory exists
+if [ ! -d "$DIRECTORY" ]; then
+  echo "Directory '$DIRECTORY' does not exist. Creating it..."
+  mkdir -p "$DIRECTORY"
+fi
+
+echo "Using directory: $DIRECTORY"
+
+# Execute the program with the provided arguments
+exec /tmp/codecrafters-build-http-server-csharp/codecrafters-http-server --directory "$DIRECTORY"
